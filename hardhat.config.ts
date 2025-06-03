beb742255d05cada988c5e7d5ff25ba5079e8b19
anonymoussprocket <anonymoussprocket@gmail.com>
2025-06-03 09:56:18 -0400

    - add back some of the original hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,15 @@
+import dotenv from 'dotenv';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || "";
+const POLYGON_AMOY_RPC_URL = process.env.POLYGON_AMOY_RPC_URL || "";
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "";
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const REPORT_GAS = process.env.REPORT_GAS !== undefined;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -11,6 +21,39 @@ const config: HardhatUserConfig = {
       },
       viaIR: true
     }
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: POLYGON_MAINNET_RPC_URL,
+        blockNumber: 67312309
+      },
+      loggingEnabled: false,
+      allowUnlimitedContractSize: true
+    },
+    polygon: {
+      url: POLYGON_MAINNET_RPC_URL,
+      accounts: [MAINNET_PRIVATE_KEY],
+      chainId: 137,
+    },
+    amoy: {
+      url: POLYGON_AMOY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 421613,
+    },
+  },
+  gasReporter: {
+    enabled: REPORT_GAS,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+  etherscan: {
+    apiKey: {
+      polygon: POLYGONSCAN_API_KEY,
+      polygonAmoy: POLYGONSCAN_API_KEY,
+    },
   }
 };
 
